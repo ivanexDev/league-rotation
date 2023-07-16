163e66cf4bb0b443a19e15a4ad6154730ca14925
Ivan Pereira <ivanexdev@gmail.com>
2023-07-15 20:53:13 -0400

    fix api url

--- src/API/champList.ts
+++ src/API/champList.ts
@@ -1,14 +1,13 @@
 const getChampionInfo = async () => {
-	try {
-		const url =
-			"http://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/champion.json";
+  try {
+    const url = 'https://ddragon.leagueoflegends.com/cdn/12.16.1/data/en_US/champion.json';
 
-		const res = await fetch(url);
-		const data = await res.json();
-		return data.data;
-	} catch (error) {
-		console.log(error);
-	}
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getChampionInfo;
