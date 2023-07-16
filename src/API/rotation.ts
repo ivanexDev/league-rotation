e864a3e38d7cdb05e8544ceb3d5fc990872329f2
Ivan Pereira <ivanexdev@gmail.com>
2023-07-15 20:44:30 -0400

    change key

--- src/API/rotation.ts
+++ src/API/rotation.ts
@@ -1,16 +1,16 @@
 const getRotation = async () => {
-	try {
-		const url =
-			"https://la2.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-f0550c8d-9902-49b4-bc3b-16bdac9e4167";
-		const res = await fetch(url);
+  try {
+    const url =
+      'https://la2.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-11a78906-10c5-4938-90df-89906b1751a7';
+    const res = await fetch(url);
 
-		const data = await res.json();
-		const rotation = data.freeChampionIds;
+    const data = await res.json();
+    const rotation = data.freeChampionIds;
 
-		return rotation;
-	} catch (error) {
-		console.log(error);
-	}
+    return rotation;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getRotation;
